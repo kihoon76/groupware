48dd07ba36ac6b3081e5033a16655fb128df116a
namkihoon <lovedeer118@gmail.com>
2019-04-30 16:22:09 +0900

    android

--- src/test/java/co/kr/drpnd/PushTest.java
+++ src/test/java/co/kr/drpnd/PushTest.java
@@ -38,7 +38,7 @@ public class PushTest {
 	
 	@Test
 	public void asyncTest() throws InterruptedException {
-		List<String> devices = sawonService.getSawonDevices("1");
+		List<String> devices = sawonService.getSawonDevices("27");
 		
 		try {
 			Map<String, String> m = new HashMap<>();
